0105643746f177da0009198903fa862900653bfe
Edilson Hdz <edilson.hdz@gmail.com>
2019-11-21 20:57:14 -0600

    first commit

---
+++ src/main/java/com/example/demosecurityjpa/DemoSecurityJpaApplication.java
@@ -0,0 +1,13 @@
+package com.example.demosecurityjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSecurityJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSecurityJpaApplication.class, args);
+	}
+
+}
